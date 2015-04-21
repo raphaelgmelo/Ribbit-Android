a97d8b00e4349b6daef89aa27cc140690cd68226
Raphael Melo <raphaelgmelo@gmail.com>
2015-04-21 11:28:33 -0300

    implemented image and video view

--- app/src/main/java/com/raphaelgmelo/ribbit/MainActivity.java
+++ app/src/main/java/com/raphaelgmelo/ribbit/MainActivity.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import android.app.AlertDialog;
@@ -31,10 +32,15 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
 import com.parse.ParseAnalytics;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
 
@@ -249,6 +255,7 @@ public class MainActivity extends ActionBarActivity implements ActionBar.TabList
                             .setTabListener(this));
         }
     }
+
 
     // receives the code from the photo or video activity
     @Override
